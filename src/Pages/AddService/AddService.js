e62cee37beb718841116a000b6bebc1569162b32
sourav-mahmud <souravmahmud32@gmail.com>
2022-11-10 23:22:23 +0600

    updated

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -49,14 +49,10 @@ const AddService = () => {
 
           <div className="flex items-center mb-5">
             <label className="inline-block w-40 mr-6 text-right font-bold text-gray-600">
-              Price
+              Comment
             </label>
-            <input
-              type="text"
-              name="price"
-              placeholder="price"
-              className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none"
-            />
+            <textarea type="text"
+              name="price" className="textarea flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" placeholder="your opinion"></textarea>
           </div>
 
           <div className="flex items-center mb-10">
