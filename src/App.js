fbd85d9ede994a11700587ffe2bd2ebb3006a915
sourav-mahmud <souravmahmud32@gmail.com>
2022-11-10 17:32:43 +0600

    private route added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import 'react-photo-view/dist/react-photo-view.css';
 import "./App.css";
 import router from "./Router/Routes/Routes";
 
 function App() {
 
   return (
-    <div>
+    <div className="text-center">
       <RouterProvider router={router}></RouterProvider>
       <ToastContainer></ToastContainer>
     </div>
--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const ServiceCard = ({ service }) => {
     const { _id, img, price, title, description } = service;
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="" /></figure>
+        <PhotoProvider>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <PhotoView><figure><img src={img} alt="" /></figure></PhotoView>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p>{description.length > 300 ? description.slice(0, 100) + '...' : description}</p>
@@ -17,6 +19,7 @@ const ServiceCard = ({ service }) => {
                 </div>
             </div>
         </div>
+        </PhotoProvider>
     );
 };
 
